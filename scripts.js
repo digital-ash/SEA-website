a67c7ae7c941491e8238bdbeb8e8adf177b08932
digital-ash <ashherr425@gmail.com>
2025-04-10 20:26:33 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -35,7 +35,6 @@ let titles = [
   "Fresh Prince of Bel Air",
   "Curb Your Enthusiasm",
   "East Los High",
-  "HELLOOOOO",
 ];
 // Your final submission should have much more data than this, and
 // you should use more than just an array of strings to store it all.
